54da30d2ae69228e124b55aa8e8d08a36d9567ca
Ethan Halprin <ethan.halprin@gmail.com>
2022-03-09 10:26:00 +0200

    Initial Commit

---
+++ SwiftUI-List/SwiftUI_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ListApp.swift
+//  SwiftUI-List
+//
+//  Created by Ethan on 09/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
