6f06cc671db54eaba46abe6a918ef73dd14be73b
Ethan Halprin <ethan.halprin@gmail.com>
2022-04-04 14:09:34 +0300

    Supplying a countdwon timer and default image for the avatar downloading progress view

--- SwiftUI-List/View/EmployeeView.swift
+++ SwiftUI-List/View/EmployeeView.swift
@@ -11,13 +11,24 @@ struct EmployeeView: View {
     var name: String
     var role: String
     var image: String
+    @State private var timeRemaining = 3
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     
     var body: some View {
         HStack {
-            AsyncImage(url: URL(string: image)) { image in
-                image.resizable()
+            AsyncImage(url: URL(string: "http://www.example.com")) { image in
+                image
+                    .resizable()
+                    .clipShape(Circle())
             } placeholder: {
-                ProgressView()
+                if timeRemaining == 0 {
+                    Image("default-female-avatar")
+                        .resizable()
+                        .frame(width: 44, height: 44)
+                } else {
+                    ProgressView()
+                }
             }
             .frame(width: 34, height: 34)
             VStack(alignment: .leading) {
@@ -27,6 +38,10 @@ struct EmployeeView: View {
                 Text("\(role)")
                     .font(.system(size: 13, weight: .medium, design: .default))
                     .foregroundColor(Color(.systemIndigo))
+            }
+        }.onReceive(timer) { time in
+            if timeRemaining > 0 {
+                timeRemaining -= 1
             }
         }
     }
